aeb3bfecf341b50a132041059d3245ad98380fb9
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-12-20 18:11:27 +0100

    added Model + migration

---
+++ database/migrations/2022_12_20_165334_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('image')->nullable()->default('http://www.multiversitycomics.com/wp-content/themes/mvc/images/timthumb.php?src=http://multiversitystatic.s3.amazonaws.com/uploads/2013/10/DC-Logo.png&q=95&w=288&zc=1&a=t');
+            $table->string('price', 20);
+            $table->string('series', 100);
+            $table->date('sale_date')->nullable();
+            $table->string('type', 20);
+            $table->string('artist', 100);
+            $table->string('writer', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
