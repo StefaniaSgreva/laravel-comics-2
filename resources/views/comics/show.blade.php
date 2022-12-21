d78362f34f84d5db209c6b27af5ae30ce0b55cfc
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-12-21 16:14:02 +0100

    added some quick infos in show.comics

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,6 +1,60 @@
 @extends('layouts.app')
 @section('main_content')
-    <section class="container">
-    <h1>{{$comic->title}}</h1>
+   
+    <section id="comic-infos">
+        <div class="comic-jumbotron">
+            <img src="{{$comic->image}}" alt="{{$comic->title}}">
+        </div>
+        <div class="blue_divider">
+            <div class="comic-cover">
+                <img src="{{$comic->image}}" alt="{{$comic->title}}">
+            </div>
+        </div>
+
+        <div class="container pt-5 pb-3">
+            <h1>{{$comic->title}}</h1>
+            <div class="info-top d-flex justify-content-center align-items-center">
+                <div class="description me-3">
+                    <p>{{$comic->description}}</p>
+                </div>
+                <div class="advertisement">
+                    <h4>Advertisement</h4>
+                    <img src="{{Vite::asset('resources/img/adv.jpg')}}" alt="Advertisement">
+                </div>
+            </div>
+        </div>
+
+        <div class="info-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="talent me-5 col">
+                        <h2>Talent</h2>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span>Art by:</span>
+                            <span>{{$comic->artist}}</span>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span>Written by:</span>
+                            <span>{{$comic->writer}}</span>
+                        </div>
+                    </div>
+                    <div class="specs col">
+                        <h2>Specs</h2>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span>Series:</span>
+                            <span>{{$comic->series}}</span>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span>U.S. Price:</span>
+                            <span>{{$comic->price}}</span>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span>On Sale Date:</span>
+                            <span>{{$comic->sale_date}}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
